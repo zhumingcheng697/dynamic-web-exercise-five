254b423bc1f15e0df0a3c897e6ff9da328dc2702
McCoy Zhu <mz1945@nyu.edu>
2020-11-24 11:17:13 -0800

    componenets and containers

---
+++ src/components/CreateAccountForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateAccountForm() {
+  return <div></div>;
+}
+
+export default CreateAccountForm;
---
+++ src/components/UserProfileComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserProfileComponent() {
+  return <div></div>;
+}
+
+export default UserProfileComponent;
